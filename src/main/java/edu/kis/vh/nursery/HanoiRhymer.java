807efaf22543a30bac326eec02b093a4f82e96d7
Mariia <252504@edu.p.lodz.pl>
2025-03-14 10:47:37 +0100

    Blad

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,30 +1,17 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymerZmieniona extends defaultCountingOutRhymer {
 
     int totalRejected = 0;
 
-<<<<<<< HEAD
-public int reportRejected() {
-    return totalRejected;
-}
-
-public void countIn(int in) {
-if (!callCheck() && in > peekaboo())
-        totalRejected++;
-        else
-            super.countIn(in);
-}
-=======
-    public int reportRejected() {
+    public int reportRejectedPoprawiona() {
         return totalRejected;
     }
 
-    public void countIn(int in) {
+    public void countInPoprawiona(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
             super.countIn(in);
     }
->>>>>>> origin/format
 }
