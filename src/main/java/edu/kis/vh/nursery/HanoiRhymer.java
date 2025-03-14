42cba69826a432d09f639a66a726271d1b717dec
Mariia <252504@edu.p.lodz.pl>
2025-03-14 11:10:55 +0100

    5. Poprawa nazw pól

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,20 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    int totalRejectedModify = 0;
 
-<<<<<<< HEAD
-public int reportRejected() {
-    return totalRejected;
-}
-
-public void countIn(int in) {
-if (!callCheck() && in > peekaboo())
-        totalRejected++;
-        else
-            super.countIn(in);
-}
-=======
     public int reportRejected() {
         return totalRejected;
     }
@@ -26,5 +14,4 @@ if (!callCheck() && in > peekaboo())
         else
             super.countIn(in);
     }
->>>>>>> origin/format
 }
