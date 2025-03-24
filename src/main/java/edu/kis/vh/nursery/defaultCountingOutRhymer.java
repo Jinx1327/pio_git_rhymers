952f29b2ff0b3ecba8c450e6eec8694101c4a3b0
Mariia <252504@edu.p.lodz.pl>
2025-03-24 14:55:04 +0100

    9. Zmiana modyfikatorow public na private

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,9 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int EMPTY = -1;
-    public static final int DEFAULT_VALUE = -1;
+    private static final int MAX_SIZE = 12;
+    private static final int EMPTY = -1;
+    private static final int DEFAULT_VALUE = -1;
 
 
     private final int[] NUMBERS = new int[MAX_SIZE];
